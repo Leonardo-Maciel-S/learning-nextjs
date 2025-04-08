c23b9075f1f43bf1881db6b4ea9e33b513b1739c
Leonardo Maciel <leonardomacielj@gmail.com>
2025-04-08 13:46:27 -0300

    feat: metadata to pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,25 @@
 import Header from "@/components/Header";
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Aula - learning NextJS",
+  description: "Learning NextJS with Sujeito Programador!",
+  openGraph: {
+    title: "Aprendendo NextJS com Sujeito Programador no Youtube!",
+    description: "Learning NextJS with Sujeito Programador!",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    noarchive: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noarchive: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
